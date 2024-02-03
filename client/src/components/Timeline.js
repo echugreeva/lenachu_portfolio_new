decd499176f5e7da4acba41f278b4b3437b3be62
echugreeva <e.chugreeva@gmail.com>
2024-02-03 18:24:44 +0200

    add experience

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -16,6 +16,25 @@ export default function Timeline() {
                 </div>
                 <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
                     <ol className="border-l-2 border-pink-900 mx-auto">
+                    <li>
+                            <div className="md:flex flex-start">
+                                <div className="bg-pink-900 w-6 h-6 flex items-center justify-center rounded-full -ml-3">
+                                    <svg aria-hidden="true" focusable="false" data-prefix="fas" className="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
+                                    </svg>
+                                </div>
+                                <div className="block-inline p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-6 mb-10 w-80">
+                                    <div className="flex justify-between mb-4">
+                                        <p href="#!" className="font-medium text-pink-900 hover:text-pink-700 focus:text-pink-800 duration-300 transition ease-in-out text-sm">Web creation software</p>
+                                        <p href="#!" className="font-medium text-pink-900 hover:text-pink-700 focus:text-pink-800 duration-300 transition ease-in-out text-sm">2023-present</p>
+                                    </div>
+                                    <p className="text-gray-700 mb-6">Full Stack Developer</p>
+                                    <p className="text-gray-700 mb-3"><span className="text-pink-700">Elementor</span></p>
+                                    <badge className="ftext-sm text-gray-700 mb-6 bg-indigo-200 px-3 py-1 rounded-full">Tel Aviv, Israel</badge>
+
+                                </div>
+                            </div>
+                        </li>
                         <li className="min-w-50">
                             <div className="md:flex flex-start">
                                 <div className="bg-pink-900 w-6 h-6 flex items-center justify-center rounded-full -ml-3">
@@ -63,7 +82,7 @@ export default function Timeline() {
                                 <div className="block-inline p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-6 mb-10 w-80">
                                     <div className="flex justify-between mb-4">
                                         <p href="#!" className="font-medium text-pink-900 hover:text-pink-700 focus:text-pink-800 duration-300 transition ease-in-out text-sm">Web creation software</p>
-                                        <p href="#!" className="font-medium text-pink-900 hover:text-pink-700 focus:text-pink-800 duration-300 transition ease-in-out text-sm">2020-present</p>
+                                        <p href="#!" className="font-medium text-pink-900 hover:text-pink-700 focus:text-pink-800 duration-300 transition ease-in-out text-sm">2020-2024</p>
                                     </div>
                                     <p className="text-gray-700 mb-6">Senior PPC Manager</p>
                                     <p className="text-gray-700 mb-3"><span className="text-pink-700">Elementor</span></p>
